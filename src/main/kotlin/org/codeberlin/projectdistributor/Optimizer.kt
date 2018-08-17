8d294a8a1277c6168d30ca798779c7f04e6cf59c
Martin Dreher <luma.inc@gmail.com>
2018-08-17 22:44:35 +0200

    more stats

--- src/main/kotlin/org/codeberlin/projectdistributor/Optimizer.kt
+++ src/main/kotlin/org/codeberlin/projectdistributor/Optimizer.kt
@@ -3,6 +3,7 @@ package org.codeberlin.projectdistributor
 import mu.KotlinLogging
 import org.codeberlin.projectdistributor.data.Data
 import org.codeberlin.projectdistributor.data.Project
+import org.codeberlin.projectdistributor.data.Role
 
 
 object Optimizer {
@@ -46,7 +47,7 @@ object Optimizer {
                     } else {
                         if (project.roles.getMax(application.role) == 0) {
                             logger.warn {
-                                "student ${user.name}, role ${application.role}, ${if (application.isMastermind)  "ProjectOwner" else "Priorität ${application.priority}"}, " +
+                                "student ${user.name}, role ${application.role}, ${if (application.isMastermind) "ProjectOwner" else "Priorität ${application.priority}"}, " +
                                         "project \"${project.name}\": ${project.roles}"
                             }
                         }
@@ -54,9 +55,22 @@ object Optimizer {
                 }
             }
 
+            val masterMindCount = user.applications.count { it.isMastermind }
+            if (masterMindCount > 1) {
+                logger.warn { "user has is a multi masterMind: ${user.name} => ${user.applications}" }
+            }
+            if (masterMindCount != 1 && user.applications.size != 3) {
+                logger.warn { "user does not have exactly 3 applications: ${user.name} => ${user.applications}" }
+            }
+
             val prioSum = user.applications.asSequence().filterNot { it.isMastermind }.sumBy { it.priority }
             if (prioSum > 10) {
                 logger.warn { "user has given out $prioSum priorities which is > 10: ${user.applications}" }
+            }
+
+            val tooLow = user.applications.filter { it.priority < 1 }
+            if (tooLow.isNotEmpty()) {
+                logger.warn { "user has has prio < 1: ${user.name} => ${user.applications}" }
             }
         }
 
@@ -66,14 +80,35 @@ object Optimizer {
     }
 
     private fun projectStats(data: Data, projects: Map<String, Project>) {
-        logger.debug("applications by project:\n${
-        data.users.asSequence()
-                .flatMap { it.applications.asSequence() }
-                .groupBy { it.project.id }.asSequence()
-                .map { project -> projects[project.key] to project.value.groupBy { it.role }.map { it.key to it.value.size } }
-                .sortedByDescending { entry -> entry.second.sumBy { it.second } }
-                .joinToString("\n") { entry -> "${entry.first} ${entry.second.sortedBy { it.first }}" }
-        }")
+        Role.values().forEach { role ->
+            logger.info {
+                val subset = projects.values.filter { it.roles.getMin(role) != it.roles.getMax(role) }
+                "applications where min$role != max$role: ${subset.size}\n${subset.joinToString("\n\t", "\t")}"
+            }
+        }
+
+        logger.debug {
+            "applications by project:\n${
+            data.users.asSequence()
+                    .flatMap { it.applications.asSequence() }
+                    .groupBy { it.project.id }.asSequence()
+                    .joinToString("\n") { (id, apps) ->
+                        val partition = apps.partition { it.isMastermind }
+                        val project = projects[id]!!
+                        listOf(
+                                project.name,
+                                project.roles.minID,
+                                project.roles.maxID,
+                                project.roles.minPM,
+                                project.roles.maxPM,
+                                project.roles.minSE,
+                                project.roles.maxSE,
+                                partition.toList().joinToString("\t") { a -> a.groupBy { it.role }.map { "${it.key}=${it.value.size}" }.sorted().joinToString() },
+                                if (project.roles.enoughApplications(partition.second)) "" else "not enough applications for ${project.roles.missingApplications(partition.second)}"
+                        ).joinToString("\t")
+                    }
+            }"
+        }
 
         logger.debug {
             "stakeholders:\n${
--- src/main/kotlin/org/codeberlin/projectdistributor/data/Roles.kt
+++ src/main/kotlin/org/codeberlin/projectdistributor/data/Roles.kt
@@ -29,4 +29,14 @@ data class Roles(
                 Role.SE -> maxSE
             }
 
+    fun enoughApplications(applications: List<Application>): Boolean {
+        val grouped = applications.groupBy { it.role }
+        return Role.values().count { getMin(it) > grouped[it]?.size ?: 0 } == 0
+    }
+
+    fun missingApplications(applications: List<Application>): List<Role> {
+        val grouped = applications.groupBy { it.role }
+        return Role.values().filter { getMin(it) > grouped[it]?.size ?: 0 }
+    }
+
 }
