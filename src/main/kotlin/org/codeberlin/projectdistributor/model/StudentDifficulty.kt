50007a5d1005b00ca6659d8cfb56798b2f441025
Martin Dreher <luma.inc@gmail.com>
2018-08-19 01:28:20 +0200

    remove warning

--- src/main/kotlin/org/codeberlin/projectdistributor/model/StudentDifficulty.kt
+++ src/main/kotlin/org/codeberlin/projectdistributor/model/StudentDifficulty.kt
@@ -1,6 +1,6 @@
 package org.codeberlin.projectdistributor.model
 
 class StudentDifficulty : Comparator<Student> {
-    val inner = compareByDescending<Student> { it.applications.size }
+    private val inner = compareByDescending<Student> { it.applications.size }
     override fun compare(a: Student?, b: Student?) = inner.compare(a, b)
 }
