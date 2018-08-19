7190d06d981587a4d9c1d1674d8750398678bb82
Martin Dreher <luma.inc@gmail.com>
2018-08-19 23:37:18 +0200

    remove unused comparator

--- src/main/kotlin/org/codeberlin/projectdistributor/model/ApplicationStrength.kt
+++
@@ -1,6 +0,0 @@
-package org.codeberlin.projectdistributor.model
-
-class ApplicationStrength : Comparator<Application> {
-    private val inner = compareBy<Application> { it.priority }
-    override fun compare(a: Application?, b: Application?): Int = inner.compare(a, b)
-}
